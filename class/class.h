e69f23b7974ba07af959379e08ac29522cc5219e
The Rendezvous 2022: Néonize <94245285+minhxiro@users.noreply.github.com>
2022-12-14 15:42:16 +0700

    Update class.h

--- class/class.h
+++ class/class.h
@@ -9,9 +9,10 @@
 using std:: string;
 using std:: cin;
 using std:: cout;
+enum location {HUE, HANOI, SAIGON}; // enum
 class House;
-class Member {
-    private:
+class User {
+    protected:
         string name;
         string full_name;
         string password;
@@ -23,9 +24,10 @@ class Member {
 
         }
     public:
-        Member(string name = "", string full_name = "",  string password = "", string phonenum = "", int credit_point = 500, int rating_score = 0)
+        User(string name = "", string full_name = "",  string password = "", string phonenum = "", int credit_point = 500, int rating_score = 0)
             : name(name), full_name(full_name), password(password), phonenum(phonenum), credit_point(credit_point), rating_score(rating_score) {}
         
+        ~User() {}
         void addHouse(House &obj) {}
         void deleteHouse(House &obj) {}
         
@@ -33,9 +35,29 @@ class Member {
 
 };
 
+
+class Admin : protected User {
+    public:
+        void showAllMember() {}
+        void viewHouseDetail() {}
+        void showAllHouse() {}
+        void viewMemberDetail() {}
+        void viewAllReQuest() {}
+        void searchHouseByCredit() {}
+        void searchHouseById() {}
+        void searchHouseByDateRange() {}
+};
+
+class Member : protected User {
+    private:
+        int memberID;
+        
+};
 class House {
     private:
-        Member owner;
+        User owner;
+        string location;
+        string description;
         int consuming_point;
         int required_score;
 };
