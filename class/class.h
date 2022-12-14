82f49002c3e95299f2a74fbb38f501f1a8070226
The Rendezvous 2022: Néonize <94245285+minhxiro@users.noreply.github.com>
2022-12-14 22:13:37 +0700

    Update class.h

--- class/class.h
+++ class/class.h
@@ -9,7 +9,7 @@
 using std:: string;
 using std:: cin;
 using std:: cout;
-enum location {HUE, HANOI, SAIGON}; // enum
+enum city {HUE, HANOI, SAIGON}; // enum
 class House;
 class User {
     protected:
@@ -27,32 +27,51 @@ class User {
         User(string name = "", string full_name = "",  string password = "", string phonenum = "", int credit_point = 500, int rating_score = 0)
             : name(name), full_name(full_name), password(password), phonenum(phonenum), credit_point(credit_point), rating_score(rating_score) {}
         
-        ~User() {}
-        void addHouse(House &obj) {}
-        void deleteHouse(House &obj) {}
         
-
+        void login();
+        void registre();
+        void showAccountInfo();
+        
+        ~User() {}
 
 };
 
 
-class Admin : protected User {
+class Admin : public User {
     public:
-        void showAllMember() {}
-        void viewHouseDetail() {}
-        void showAllHouse() {}
-        void viewMemberDetail() {}
-        void viewAllReQuest() {}
-        void searchHouseByCredit() {}
-        void searchHouseById() {}
-        void searchHouseByDateRange() {}
+        void showAllMember();
+        void viewHouseDetail();
+        void showAllHouse();
+        void viewMemberDetail();
+        void viewAllReQuest();
+        void searchHouseByCredit();
+        void searchHouseById();
+        void searchHouseByDateRange();
 };
 
-class Member : protected User {
+class Member : public User {
     private:
         int memberID;
-        
+        city location;
+        House house_list;
+    public:
+        void showAllHouse();
+        void searchHouseByRegion();
+        void reviewAllRequest();
+        void acceptReQuest();
+        void declineRequest();
+        void rateTentant();
+        void deleteHouseList();
+        void addHouseList();
+        void rateTenant();
+        void viewAllHouse();
+        void requestHouse();
+        void cancelRequest();
+        void rateHouse();
+        ~Member() {}
 };
+
+
 class House {
     private:
         User owner;
@@ -60,5 +79,10 @@ class House {
         string description;
         int consuming_point;
         int required_score;
+        city location;
+};
+
+class System {
+    
 };
 #endif
