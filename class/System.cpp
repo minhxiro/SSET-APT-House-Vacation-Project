b1ca933709e5bdc0d7470732923bdf91663941b7
Trung <ngolambaotrung@gmail.com>
2022-12-18 21:22:01 +0700

    Complete authentication and extract data function

--- class/System.cpp
+++ class/System.cpp
@@ -1,21 +1,35 @@
 
 #include <iostream>
-#include <System.h>
+#include <fstream>
+#include <sstream>
+#include "System.h"
 #include <regex>
 
 using std::string;
 using std::cout;
 using std::regex;
 
+string trimString(string str) {
+    string finalStr = "";
+    for (char ch: str) {
+        if (ch == ' ') {
+            continue;
+        } else {
+            finalStr += ch;
+        }
+    }
+    return finalStr;
+}
+
 
 bool inputUsernameAuthentication(string username) {
-    //false: cout "Username should only contains 8 to 15 character and no white spaces!!!"
+    //false: Username should only contain 8 to 15 character and no white spaces!!!
     //true: continue
-    regex reg("\s");
+    regex reg("\\s");
     if (std::regex_match(username, reg)) {
         return false;
     } else {
-        if (8 > username.length() || username.length() > 14) {
+        if (8 > username.length() || username.length() > 16) {
             return false;
         } else {
             return true;
@@ -23,10 +37,11 @@ bool inputUsernameAuthentication(string username) {
     }
 }
 
-bool inputNameAuthentication(string name) {
+bool inputNameAuthentication(string &name) {
     //false: Name must contain 8 to 20 characters and no digits, no special characters, and no white spaces
     //true: continue
     regex reg("^[a-zA-Z]*{8,20}$");
+    name = trimString(name);
     if (std::regex_match(name, reg)) {
         return true;
     } else {
@@ -34,29 +49,85 @@ bool inputNameAuthentication(string name) {
     }
 }
 
-bool inputPasswordAuthenticate(string password){
+bool inputPasswordAuthenticate(string &password) {
     //false: Minimum 8 and maximum 10 characters, at least one uppercase letter, one lowercase letter, one number and one special character:
     //true: continue
-    regex reg ("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$");
-    if(std::regex_match(password,reg)){
+    regex reg("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@$!%*?&])[A-Za-z0-9@$!%*?&]{8,10}$");
+    password = trimString(password);
+    if (std::regex_match(password, reg)) {
         return true;
-    }else{
+    } else {
         return false;
     }
 
-
 }
 
-bool inputPhoneAuthenticate(string phoneNum);
+bool inputPhoneAuthenticate(string &phoneNum) {
+    //false: phone number must have 11 numbers and start with 0
+    //true: continue
+    regex reg("^(?=0)[0-9]{10}$");
+    phoneNum = trimString(phoneNum);
+    if (std::regex_match(phoneNum, reg)) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
-bool inputNumAuthenticate(string num);
 
-bool inputRangeAuthenticate(string range);
+bool inputNumAuthenticate(string &num) {
+    //false: The input must be a number
+    //true: continue
+    regex reg("^[0-9]$");
+    num = trimString(num);
+    if (std::regex_match(num, reg)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+bool inputRangeAuthenticate(string &range) {
+    //false: Range must be in this format number - number
+    //true: continue
+    regex reg("^[0-9]+[-][0-9]+$");
+    range = trimString(range);
+    if (std::regex_match(range, reg)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 
 bool creditAuth(int credits);
 
 bool scoreAuth(int scores);
 
-int main() {
-
+vector<vector<string> > extractByRow(string dataFile) {
+    std::fstream file;
+    string dataLine;
+    std::vector<vector<string> > dataTable;
+    std::vector<string> dataRowsArray;
+    string cell;
+    file.open(dataFile, std::ios::in);
+    if (file.fail()) {
+        cout << "Cannot reach the database \n";
+    } else {
+        while (!file.eof()) {
+            std::stringstream ss;
+            dataRowsArray = {};
+            std::getline(file, dataLine);
+            ss << dataLine;
+            while (!ss.eof()) {
+                std::getline(ss, cell, ';');
+                dataRowsArray.push_back(cell);
+            }
+            dataTable.push_back(dataRowsArray);
+        }
+        file.close();
+    }
+    return dataTable;
 }
+
--- class/System.h
+++ class/System.h
@@ -8,15 +8,31 @@
 #endif //SSET_APT_HOUSE_VACATION_PROJECT_SYSTEM_H
 
 #include <iostream>
+#include <vector>
 using std::string;
+using std::vector;
 
 class System{
+    friend string trimString(string str);
     friend bool inputUsernameAuthentication(string username);
-    friend bool inputNameAuthentication(string name);
-    friend bool inputPasswordAuthenticate(string password);
-    friend bool inputPhoneAuthenticate(string phoneNum);
-    friend bool inputNumAuthenticate(string num);
-    friend bool inputRangeAuthenticate(string range);
+    friend bool inputNameAuthentication(string &name);
+    friend bool inputPasswordAuthenticate(string &password);
+    friend bool inputPhoneAuthenticate(string &phoneNum);
+    friend bool inputNumAuthenticate(string &num);
+    friend bool inputRangeAuthenticate(string &range);
     friend bool creditAuth(int credits);
     friend bool scoreAuth(int scores);
+    friend void deleteRowData(int index, string dataFile);
+    friend void updateCellAtId(int id, string data, string dataFile);
+    friend void updateRowAtIndex(int index, string data, string dataFile);
+    friend void addData(string data, string dataFile);
+    friend vector<vector<string> > extractByRow(string dataFile);
+    friend vector<string> extractByColumnIndex(int index, string dataFile);
+    friend string getCurrentDate();
+    friend int idAutoIncrement();
+    friend void searchByCredits();
+    friend void searchById();
+    friend void searchByDate();
+
+
 };
