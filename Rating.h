7eaa3e45225aac25feb1088e9353a5833691cf25
s3669698 <s3669698@rmit.edu.vn>
2023-01-05 15:45:58 +0700

    update

--- Rating.h
+++ Rating.h
@@ -6,15 +6,13 @@ using std::cout;
 class Rating
 {
 private:
-    int ratingId;
     int score;
     string comment;
-    string currentDate;
     int occupierId;
     int houseId;
 public:
-    Rating(int ratingId = 0, int score = 0, string comment = "", string currentDate = "", int occupierId = 0, int houseId = 0)
-    : ratingId(ratingId), score(score), comment(comment), currentDate(currentDate), occupierId(occupierId), houseId(houseId){}
+    Rating(int score = 0, string comment = "", int occupierId = 0, int houseId = 0)
+    : score(score), comment(comment), occupierId(occupierId), houseId(houseId){}
     void rateHouse() {}
     void rateOccupier() {}
     void showInfo() {}
